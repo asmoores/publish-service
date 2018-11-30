f1c46f74164c7e55dd9dd6f4e76dce65fc4882e5
Andrew Moores <andrew.moores@digital.homeoffice.gov.uk>
2018-11-30 22:14:19 +0000

    Add GET handler to retrieve documents.

--- src/main/java/com/agilesoftware/rest/resteasy/DocumentResource.java
+++ src/main/java/com/agilesoftware/rest/resteasy/DocumentResource.java
@@ -1,6 +1,7 @@
 package com.agilesoftware.rest.resteasy;
 
 import com.agilesoftware.rest.resteasy.utils.ResponsePrinter;
+import org.apache.commons.lang.ObjectUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +14,38 @@ import javax.ws.rs.core.Response;
 @Path("/document")
 public class DocumentResource {
     private static final Logger log = LoggerFactory.getLogger(DocumentResource.class);
+
+    @GET
+    @Path("{docId}")
+    public Response get(@PathParam("docId") String id) {
+        log.debug("Handling a docment GET request for document id {}", id);
+        Client client = ClientBuilder.newClient();
+
+        String uri = "http://localhost:9200/newindex/_doc/" + id;
+        Response responseFromNoSQLStore = null;
+        try {
+            responseFromNoSQLStore = client.target(uri).request().get();
+
+            ResponsePrinter.logResponseDetails(responseFromNoSQLStore);
+            log.debug("Call to NoSQL data store returned {}", responseFromNoSQLStore);
+
+            // Take a copy of the entity from the response before the finally clause closes the response.
+            // The return value is constructed after the finally clause is executed.  Can't build the response and store
+            // it in a local variable as the ResponseBuilder uses a reference as well.
+            Object entity = ObjectUtils.clone(responseFromNoSQLStore.getEntity());
+
+            return Response.status(Response.Status.OK).entity(entity).build();
+        } finally {
+            try {
+                log.debug("Closing resources...");
+                if (responseFromNoSQLStore != null) responseFromNoSQLStore.close();
+                if (client != null) client.close();
+                log.debug("done.");
+            } catch(Exception e) {
+                log.error("failed cleanup", e);
+            }
+        }
+    }
 
     @POST
     @Path("{index}")
@@ -40,8 +73,8 @@ public class DocumentResource {
             return Response.status(Response.Status.CREATED).build();
         } finally {
             log.debug("Closing resources...");
-            responseFromNoSQLStore.close();
-            client.close();
+            if (responseFromNoSQLStore != null) responseFromNoSQLStore.close();
+            if (client != null) client.close();
             log.debug("done.");
         }
     }
