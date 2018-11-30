59cc3290790427a2ffd1d1318e47679d2e66bea2
Andrew Moores <andrew.moores@digital.homeoffice.gov.uk>
2018-11-30 21:13:40 +0000

    Add logging, resource printer. Add debug messages.

--- src/main/java/com/agilesoftware/rest/resteasy/DocumentResource.java
+++ src/main/java/com/agilesoftware/rest/resteasy/DocumentResource.java
@@ -1,5 +1,9 @@
 package com.agilesoftware.rest.resteasy;
 
+import com.agilesoftware.rest.resteasy.utils.ResponsePrinter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.*;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -8,25 +12,37 @@ import javax.ws.rs.core.Response;
 
 @Path("/document")
 public class DocumentResource {
+    private static final Logger log = LoggerFactory.getLogger(DocumentResource.class);
 
     @POST
     @Path("{index}")
-    public void saveDocument(@PathParam("index") String index) {
+    public Response saveDocument(@PathParam("index") String index) {
+        log.debug("Handling a document POST request.");
         Client client = ClientBuilder.newClient();
 
-        String uri = "http://localhost:9200/" + index + "/_doc/3";
+        String uri = "http://localhost:9200/" + index + "/_doc";
+        Response responseFromNoSQLStore = null;
+        try {
+            responseFromNoSQLStore = client.target(uri).request().post(Entity.json("{\n" +
+                    "    \"user\" : \"andrew\",\n" +
+                    "    \"post_date\" : \"2018-11-15T14:12:12\",\n" +
+                    "    \"message\" : \"trying out Elasticsearch from resteasy\"\n" +
+                    "}"));
 
-        Response response = client.target(uri).request().post(Entity.json("{\n" +
-                "    \"user\" : \"andrew\",\n" +
-                "    \"post_date\" : \"2018-11-15T14:12:12\",\n" +
-                "    \"message\" : \"trying out Elasticsearch from resteasy\"\n" +
-                "}"));
+            ResponsePrinter.logResponseDetails(responseFromNoSQLStore);
+            log.debug("Call to NoSQL data store returned {}", responseFromNoSQLStore);
 
-        response.close();
-        client.close();
+            if (responseFromNoSQLStore.getStatus() != Response.Status.CREATED.getStatusCode()) {
+                log.debug("Converting http error from NoSQL API to a WebApplicationException");
+                throw new WebApplicationException("Could not create resource", Response.Status.INTERNAL_SERVER_ERROR);
+            }
 
-        if (response.getStatus() != Response.Status.CREATED.getStatusCode()) {
-            throw new RuntimeException("could not create resource");
+            return Response.status(Response.Status.CREATED).build();
+        } finally {
+            log.debug("Closing resources...");
+            responseFromNoSQLStore.close();
+            client.close();
+            log.debug("done.");
         }
     }
 }
---
+++ src/main/java/com/agilesoftware/rest/resteasy/utils/ResponsePrinter.java
@@ -0,0 +1,17 @@
+package com.agilesoftware.rest.resteasy.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.core.Response;
+
+public class ResponsePrinter {
+    private static final Logger log = LoggerFactory.getLogger(ResponsePrinter.class);
+
+    public static void logResponseDetails(Response response) {
+        log.debug("Reponse is: \n {} \n {}",
+                response.getStatus(),
+                response.getStatusInfo());
+
+    }
+}
