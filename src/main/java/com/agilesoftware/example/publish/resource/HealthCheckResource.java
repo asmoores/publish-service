7a4d95e9716cf70dc8304346bc9b0b7f24133846
Andrew Moores <a.s.moores@gmail.com>
2019-08-25 09:56:55 +0100

    Correct typo.

--- src/main/java/com/agilesoftware/example/publish/resource/HealthCheckResource.java
+++ src/main/java/com/agilesoftware/example/publish/resource/HealthCheckResource.java
@@ -6,7 +6,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 @Path("/health")
-public class HeatlthCheckResource {
+public class HealthCheckResource {
     @GET
     @Produces("application/text")
     public Response health() {
